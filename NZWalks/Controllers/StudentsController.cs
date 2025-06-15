9a5f62bab246324f188971e2e833568495130211
Aida Koorang Beheshti <aida.beheshti81@gmail.com>
2025-06-15 09:16:30 +0330

    create new models and database

---
+++ NZWalks/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStudets()
+        {
+            var students = new List<string> { "John Doe", "Jane Smith", "Alice Johnson" };
+            return Ok(students);
+        }
+    }
+
+}
---
+++ NZWalks/Models/Domain/Walk.cs
@@ -0,0 +1,20 @@
+﻿namespace NZWalks.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+
+        public string? WalkImageUrl { get; set; }
+
+        // foreign keys for region and difficulty(from naming)
+        public Guid RegionId { get; set; }
+        public Guid DifficultyId { get; set; }
+
+        // for reading data return the whole class not just id
+        public Region Region { get; set; }
+        public Difficulty Difficulty { get; set; }
+    }
+}
