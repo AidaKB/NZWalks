1850128e8a6f055bed182fd89359e8aca854681c
Aida Koorang Beheshti <aida.beheshti81@gmail.com>
2025-06-15 11:33:31 +0330

    add Region DTO for Region Model

--- NZWalks/Controllers/RegionsController.cs
+++ NZWalks/Controllers/RegionsController.cs
@@ -18,8 +18,44 @@ namespace NZWalks.Controllers
         [HttpGet]
         public IActionResult GetAllRegions()
         {
-            var regions = dbContext.Regions.ToList();
-            return Ok(regions);
+            var regionsDomain = dbContext.Regions.ToList();
+            var regionsDtos = new List<Models.DTO.RegionDto>();
+            foreach (var region in regionsDomain)
+            {
+                var regionDto = new Models.DTO.RegionDto
+                {
+                    Id = region.Id,
+                    PresentationName = region.Name,
+                    Code = region.Code,
+                    RegionImageUrl = region.RegionImageUrl
+                };
+                regionsDtos.Add(regionDto);
+            }
+
+            return Ok(regionsDtos);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            //var region = dbContext.Regions.Find(id);
+            var regionDomain = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+            if (regionDomain == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var regionDto = new Models.DTO.RegionDto
+                {
+                    Id = regionDomain.Id,
+                    PresentationName = regionDomain.Name,
+                    Code = regionDomain.Code,
+                    RegionImageUrl = regionDomain.RegionImageUrl
+                };
+                return Ok(regionDto);
+            }
         }
             
     }
