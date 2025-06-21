d2572a903caf2f56e3278446c489c6a2d625553b
Aida Koorang Beheshti <aida.beheshti81@gmail.com>
2025-06-21 15:04:04 +0330

    inject auth db context and seed role

---
+++ NZWalks/Data/NZWalksAuthDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NZWalks.Data
+{
+    public class NZWalksAuthDbContext : IdentityDbContext
+    {
+        public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
+        {
+
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "5f7b757c-d73e-43b1-b7dc-9dbbf050a6b5";
+            var writerRoleId = "83a88791-a22f-4d2f-9381-3a5b2fcc38b7";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                
+                
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                },
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+    }
+}
